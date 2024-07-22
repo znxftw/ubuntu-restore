0689a10f814815c945b6683c55a2898caba019a4
znxftw <vishnu2101@gmail.com>
2024-07-22 23:04:00 +0530

    move to central script - will be adding themes later

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo apt upgrade -y -qq
 
 echo "[::DEBUG] Installing packages"
 
+# .deb packages
 fetch_and_install bitwarden
 fetch_and_install code
 fetch_and_install discord
@@ -18,7 +19,9 @@ fetch_and_install steam
 fetch_and_install xclip
 fetch_and_install zsh   
 
-# Installing oh-my-zsh
+# oh-my-zsh
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
     sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 fi
+
+
